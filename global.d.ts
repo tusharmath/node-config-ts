a3ed4289574d253e8ce9244fc1656c4131de53fa
Tushar Mathur <tusharmath@gmail.com>
2018-01-10 08:12:02 +0530

    fix(tsc): use module-exists typings instead of internally defined

--- global.d.ts
+++ global.d.ts
@@ -2,11 +2,6 @@
  * Created by tushar on 30/12/17.
  */
 
-declare module 'module-exists' {
-  function exists(path: string): boolean
-  export = exists
-}
-
 declare module '*.json' {
   export {}
 }
