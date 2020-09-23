7f601d46c772cdca671dfb9fd30f085e80380130
Alex Dransfield <alex.dransfield@gmail.com>
2020-09-23 19:04:53 +0100

    feat: adding `NODE_CONFIG_TS_ENV` as an alternative to `NODE_ENV`
    fixes #64

--- test/mergeAllConfigs.ts
+++ test/mergeAllConfigs.ts
@@ -61,4 +61,65 @@ describe('mergeAllConfigs()', () => {
     }
     assert.deepEqual(actual, expected)
   })
+
+  describe('alternative env varialble', () => {
+    it('should load configs from all the places', () => {
+      const process = {
+        argv: [],
+        cwd: () => path.resolve(__dirname, 'stub-module'),
+        env: {
+          DEPLOYMENT: 'www.example.com',
+          NODE_CONFIG_TS_ENV: 'production',
+          USER: 'root',
+          MAX_RETRIES: 999
+        }
+      }
+      const actual = mergeAllConfigs(process)
+      const expected = {
+        type: 'user',
+        port: 9000,
+        maxRetries: 999
+      }
+      assert.deepEqual(actual, expected)
+    })
+    it('should override with cli configs', () => {
+      const process = {
+        argv: ['--port', '3000', '--wonder', 'woman'],
+        cwd: () => path.resolve(__dirname, 'stub-module'),
+        env: {
+          DEPLOYMENT: 'www.example.com',
+          NODE_CONFIG_TS_ENV: 'production',
+          USER: 'root',
+          MAX_RETRIES: 999
+        }
+      }
+      const actual = mergeAllConfigs(process)
+      const expected = {
+        type: 'user',
+        port: 3000,
+        wonder: 'woman',
+        maxRetries: 999
+      }
+      assert.deepEqual(actual, expected)
+    })
+    it('should override ENV variables with cli configs', () => {
+      const process = {
+        argv: ['--port', '3000', '--maxRetries', '150'],
+        cwd: () => path.resolve(__dirname, 'stub-module'),
+        env: {
+          DEPLOYMENT: 'www.example.com',
+          NODE_CONFIG_TS_ENV: 'production',
+          USER: 'root',
+          MAX_RETRIES: 999
+        }
+      }
+      const actual = mergeAllConfigs(process)
+      const expected = {
+        type: 'user',
+        port: 3000,
+        maxRetries: 150
+      }
+      assert.deepEqual(actual, expected)
+    })
+  })
 })
