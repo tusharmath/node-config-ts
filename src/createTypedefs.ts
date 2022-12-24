7cd029368b7e8df9ad49b66643a83c0f9f24ab35
Don Isaac <donald.isaac@gmail.com>
2022-12-24 00:28:10 -0500

    test: add test suite for typedef generation (#72)
    * test: add typedef tests
    
    * chore: code cleanup

--- src/createTypedefs.ts
+++ src/createTypedefs.ts
@@ -6,28 +6,9 @@ import * as fs from 'fs'
 import * as path from 'path'
 import {config} from '../index'
 import {baseConfigPath} from './baseConfigPath'
-import * as prettier from 'prettier'
-
-const JsonToTS = require('json-to-ts')
+import {createTypedefCode} from './createTypedefCode'
 
 const file = `Config.d.ts`
-const ts = prettier.format(
-  [
-    '/* tslint:disable */',
-    '/* eslint-disable */',
-    'declare module "node-config-ts" {'
-  ]
-    .concat(JsonToTS(config, {rootName: 'IConfig'}))
-    .concat([
-      'export const config: Config',
-      'export type Config = IConfig',
-      '}'
-    ])
-    .join('\n'),
-  {
-    parser: 'typescript',
-    semi: false
-  }
-)
+const ts = createTypedefCode(config)
 
 fs.writeFileSync(path.resolve(process.cwd(), baseConfigPath(process), file), ts)
