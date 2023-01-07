ec907415ccc5ec45009832a4f9b046b47377d221
miyatrayash <yash.miyatra@jeavio.com>
2023-01-08 00:15:13 +0530

    Added dotenv support

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 /**
  * Created by tushar on 30/12/17.
  */
-
+import * as dotenv from 'dotenv'
 import {mergeAllConfigs} from './src/mergeAllConfigs'
+
+dotenv.config()
 export const config = mergeAllConfigs(process)
