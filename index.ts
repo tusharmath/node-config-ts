b7d67312ce7ccce3ea47ebc5ba91fa0ea3afae7a
Don Isaac <donald.isaac@gmail.com>
2023-01-08 14:07:51 -0500

    fix: make dotenv optional

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 /**
  * Created by tushar on 30/12/17.
  */
-import * as dotenv from 'dotenv'
 import {mergeAllConfigs} from './src/mergeAllConfigs'
 
-dotenv.config()
+// Try to load .env file. This will fail if dotenv is not installed. Note that
+// require() is used instead of import() because import() is asynchronous and we
+// need to load the config before the app starts. Also, ES6 import statements
+// can't be wrapped in a try/catch block.
+try {
+    require('dotenv').config()
+} catch (err) {
+    // ignore
+}
+
 export const config = mergeAllConfigs(process)
